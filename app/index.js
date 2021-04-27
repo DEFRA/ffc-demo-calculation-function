c83663c2e9d62329b5ee2777eccc4b26da2114d8
John Watson <johnwatson484@hotmail.com>
2021-04-27 21:55:43 +0100

    Simplify function

--- app/index.js
+++ app/index.js
@@ -1,14 +1,15 @@
 const calculationService = require('./services/calculation-service')
 const protectiveMonitoringSendEvent = require('./services/protective-monitoring-service')
-const sendCalculation = require('./messaging/send-calculation')
 
 module.exports = async function (context, message) {
   try {
     const claim = message
     if (claim.claimId !== undefined) {
       const value = calculationService.calculate(context, claim)
-      await sendCalculation(context, { claimId: claim.claimId, value })
+      context.bindings.outputSbQueue = { claimId: claim.claimId, value }
       await protectiveMonitoringSendEvent(claim.claimId, 'Claim calculated')
+      context.log.info(`Published payment for ${claim.claimId}`)
+      context.done()
     }
   } catch (err) {
     context.log.error('Unable to process message:', err)
--- app/messaging/create-message.js
+++
@@ -1,9 +0,0 @@
-function createMessage (payment) {
-  return {
-    body: payment,
-    type: 'uk.gov.demo.claim.calculated',
-    source: 'ffc-demo-calculation-function'
-  }
-}
-
-module.exports = createMessage
